ae60c720f8638f1329e7d910c6f59f02573c264d
weaverth <weaverth@bc.edu>
2021-09-27 10:59:21 -0400

    5.6 Done!

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Teddy Weaver on 9/27/21.
+//
+
+import Foundation
+
+struct ToDoItem{
+    var name: String
+    var date: Date
+    var notes: String
+}
