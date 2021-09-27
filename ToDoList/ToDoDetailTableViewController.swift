27d34443c9d11eb63ee043afca4f39556abb591a
weaverth <weaverth@bc.edu>
2021-09-26 22:19:12 -0400

    5.3 Done!

---
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by Teddy Weaver on 9/26/21.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else{
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
